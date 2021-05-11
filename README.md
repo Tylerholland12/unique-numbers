1b2deff9bb488fb67f3228b8eee347b4bf697bbc
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-05-11 12:09:32 -0500

    [initial]

---
+++ README.md
@@ -0,0 +1,1 @@
+npm i unique-number-generator-math
---
+++ index.js
@@ -0,0 +1,6 @@
+var arr = [];
+while(arr.length < 8){
+    var r = Math.floor(Math.random() * 100) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
